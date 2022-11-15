11d625cebc78ee7047eb42bf162ef97afd890ee5
cgs <2245492326@qq.com>
2022-11-15 14:08:44 +0800

    hot1

--- src/main/java/com/helloGit.java
+++ src/main/java/com/helloGit.java
@@ -4,5 +4,6 @@ public class helloGit {
     public static void main(String[] args) {
         System.out.println("你好Git");
         System.out.println("你好git2");
+        System.out.println("你好hot");
     }
 }
